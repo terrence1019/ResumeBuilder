97de1e73e9bfe127d39156b0087414d9d7891e3a
Terrence John <terrence1019@outlook.com>
2022-05-04 11:10:13 -0400

    • Add ApplicantsController

---
+++ Controllers/ApplicantsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RésuméBuilder.Controllers
+{
+    public class ApplicantsController : Controller
+    {
+        // GET: Applicants
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //BASIC LIST OF USERS IN DATABASE
+        //ListOfApplicantsPageView()
+        //ViewApplicantDetails()
+
+
+        //REGISTERING A USER FOR RESUME SERVICE
+        //RegisterUserPageView()
+
+
+        //RegisterUserFormAction()
+
+
+
+        //RESUME CREATION
+        //CreateApplicationPageView()
+        //CreateApplicationFormAction()
+
+
+
+
+    }
+}
