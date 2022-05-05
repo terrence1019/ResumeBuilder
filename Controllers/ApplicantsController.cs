beec60a2db2db39f81f91dbec937f986d444fb61
Terrence John <terrence1019@outlook.com>
2022-05-04 23:22:25 -0400

    • Add ApplicantsController Actions • Improve Navigation

--- Controllers/ApplicantsController.cs
+++ Controllers/ApplicantsController.cs
@@ -4,34 +4,115 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+//Required to use Models
+using RésuméBuilder.Models;
+
+
+
+
 namespace RésuméBuilder.Controllers
 {
     public class ApplicantsController : Controller
     {
-        // GET: Applicants
-        public ActionResult Index()
+        
+
+        //QUERYING A DATABASE
+        //[QUERRYING A DATABASE] STEP 01:
+        //Need to query the Applicants database table?
+        //Place an instance of MyDbContext in Applicants controller
+        private MyDbContext dbContext;
+
+
+        //[QUERRYING A DATABASE] STEP 02:
+        //Initialize dbContext via CustomerController class constructor
+        public ApplicantsController()
+        {
+            dbContext = new MyDbContext();
+        }
+
+
+        //[QUERRYING A DATABASE] STEP 03:
+        //Ensure to properly dispose of instance by overriding the Dispose() method of the Base Controller class
+        protected override void Dispose(bool disposing)
+        {
+            dbContext.Dispose();
+        }
+
+
+
+
+        //BASIC LIST OF USERS IN DATABASE
+        //ListOfApplicantsPageView()
+        public ActionResult ListOfApplicantsPageView()
+        {
+            var applicantTable = dbContext.applicantDB;
+
+            //Counts the number of records in Applicant Table to determine if empty
+            if (applicantTable.Count() == 0)
+                return View("NoApplicants");
+
+
+            return View(applicantTable);
+        }
+
+
+        //REGISTERING A USER FOR RESUME SERVICE
+        //RegisterUserPageView()
+        public ViewResult RegisterUserPageView()
         {
             return View();
         }
 
 
-        //BASIC LIST OF USERS IN DATABASE
-        //ListOfApplicantsPageView()
-        //ViewApplicantDetails()
-
-
-        //REGISTERING A USER FOR RESUME SERVICE
-        //RegisterUserPageView()
-
-
         //RegisterUserFormAction()
+        //ACTION FOUND IN VIEW FOR FORM.
+        //USED FOR FORM OPERATIONS IN CONJUNCTION WITH DATABASE (MODEL BINDING)
+        [HttpPost]
+        public ActionResult RegisterUserFormAction(Applicant applicant)
+        {
+            var applicantTable = dbContext.applicantDB;
 
 
+            //AUTO-INCREMENT ApplicantID
+            //Count the number of pre-existing records
+            //Casting to byte required due to DataType of ApplicantID
+            int PreExistingRecords = applicantTable.Count();
+            int AutoIncrementID = ++PreExistingRecords;
+
+            //Auto-Increment ApplicantID by 1 for the new member
+            applicant.ApplicantID = AutoIncrementID;
+
+            //Test Correctness of Data Entry:
+            //var name = applicant.ApplicantUsername;
+
+            //Add Applicant Record to Applicant Table of DB
+            applicantTable.Add(applicant);
+
+            //Save Changes to Database / Database Context
+            dbContext.SaveChanges();
+
+            return View("SuccessfullyRegistered");
+        }
+
+        // ATTRIBUTE ROUTING
+        [Route("Applicants/ApplicantDetails/{applicantID}")]
+        public ActionResult ApplicantDetails (int applicantID)
+        {
+
+            var val = applicantID;
+
+            var applicantRecord = dbContext.applicantDB.SingleOrDefault(a => a.ApplicantID == applicantID);
+
+            if (applicantRecord == null) return HttpNotFound();
+
+            return View();
+        }
 
         //RESUME CREATION
         //CreateApplicationPageView()
-        //CreateApplicationFormAction()
-
+        //CreateApplicationFormAction(applicantID)
+        //ViewApplicantDetails(applicantID)
+        //ViewAppicationDetails(applicantID)
 
 
 
