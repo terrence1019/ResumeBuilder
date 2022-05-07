ba7b737a18e6a8f9fcf91929ed05b43e6f607450
Terrence John <terrence1019@outlook.com>
2022-05-06 20:47:39 -0400

    • UI Improvements

---
+++ ViewModels/AddPersonalsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//I need to use two Models: Applicant and Personal
+using RésuméBuilder.Models;
+
+namespace RésuméBuilder.ViewModels
+{
+    public class AddPersonalsViewModel
+    {
+
+        //An instance of Applicant Model:
+        public Applicant ApplicantRec { get; set; }
+
+        //An instance of Personal Model:
+        public Personal PersonalRec { get; set; }
+
+    }
+}
