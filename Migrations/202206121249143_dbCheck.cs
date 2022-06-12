02387c834d1abc228b1ebb29ca0190d608cac4bf
Terrence John <terrence1019@outlook.com>
2022-06-12 10:03:02 -0400

    • Continued improvement for adding Job Experience

---
+++ Migrations/202206121249143_dbCheck.cs
@@ -0,0 +1,16 @@
+﻿namespace RésuméBuilder.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class dbCheck : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
