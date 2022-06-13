f9e73323d5761714903b6a9b5b0a3620e4e1d03f
Terrence John <terrence1019@outlook.com>
2022-06-13 14:06:48 -0400

    • Major work on Add Job data to Database

--- Controllers/JobsController.cs
+++ Controllers/JobsController.cs
@@ -52,6 +52,88 @@ namespace RésuméBuilder.Controllers
 
 
 
+        //POSTING ACTIONS: POST TO JOB TABLE
+        [HttpPost]
+        public ActionResult AddJobFormActionQuery
+            (int applicantID, string jobNumberID, string companyName, string position, string fromDate, string toDate)
+        {
+
+
+
+
+
+            return View();
+
+        }
+
+        //POSTING ACTIONS: POST TO JOBDETAIL TABLE
+        [HttpPost]
+        public ActionResult AddJobDetailsFormActionQuery
+            (int applicantID, string jobNumberID, string [] jobFunctionsList)
+        {
+
+
+            //PULL UP JOB DETAILS TABLE
+
+
+
+
+            //GET ARRAY ELEMENTS
+            var test0 = jobFunctionsList[0];
+            Console.WriteLine(test0);
+
+            var test1 = jobFunctionsList[1];
+            Console.WriteLine(test1);
+
+
+            int i = 0;
+
+            int len = jobFunctionsList.Length;
+
+            //ADD EACH JOB FUNCTION TO JOB DETAIL TABLE
+            for (i = 0; i < len; i++)
+            {
+                //Console.WriteLine( jobFunctionsList[i] );
+                                
+
+                string JobID = jobNumberID;
+                string JobFunctionID = jobNumberID + "_detail_" + i;
+
+
+                //Create instance of JobDetail Model/Class/Table
+                //NOTE: Instance of JobDetail Model/Class/Table = Table Record in JobDetail
+                JobDetail jobdetailItem = new JobDetail();
+
+                /* What's going to be required to be saved to the database?
+                 * ApplicantID
+                 * JobID
+                 * JobFunctionID
+                 * JobFunction
+                */
+                jobdetailItem.ApplicantID = applicantID;
+                jobdetailItem.JobID = JobID;
+                jobdetailItem.JobFunctionID = JobFunctionID;
+                jobdetailItem.JobFunction = jobFunctionsList[i];
+
+                //Pull Up JobDetail DB Table:
+                var jobdetailsTable = dbContext.jobdetailDB;
+
+                //Add Each Job Detail Item to JobDetail DB Table:
+                jobdetailsTable.Add(jobdetailItem);
+
+                //Save Changes to DB Table:
+                //dbContext.SaveChanges();
+
+            }
+
+
+
+
+            Console.WriteLine();
+
+            return View();
+
+        }
 
 
 
